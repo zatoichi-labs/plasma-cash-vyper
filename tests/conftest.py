6fb02c84b83f8865e4e1a379d2f2570c1770558a
Bryant Eisenbach <fubuloubu@gmail.com>
2018-12-03 20:43:51 -0500

    Trivial implementation of spec

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,57 +1,73 @@
 import pytest
-from eth_utils import keccak, to_int, to_bytes
-
-from plasma_cash import Operator, User
 
 
-NUM_TOKENS=64  # Maximum our contract takes for initilizer
+class Token:
+    exit_started = None
 
-
-@pytest.fixture
-def purse(w3):
-    uid = lambda i, a: to_int(keccak(to_bytes(hexstr=a) + to_bytes(i)))
-    purse = {}
-    users = w3.eth.accounts[1:]
-    tokens_per_user = NUM_TOKENS//len(users)
-    # Psuedo-randomly distribute tokens to users
-    for a in users:
-        # Give each account 7 tokens with random IDs
-        uids = [uid(i, a) for i in range(tokens_per_user)]
-        purse[a] = uids
-    # Make sure we have exactly NUM_TOKENS distributed
-    distributed = (tokens_per_user*len(users))
-    for i in range(NUM_TOKENS-distributed):
-        a = users[i]
-        purse[a].append(uid(tokens_per_user, a))
-    return purse
+    def __init__(self, uid):
+        self.uid = uid
 
 
 @pytest.fixture
-def token(vy_deployer, purse):
-    # Starting balances is a list of tokens and users who own them
-    tokens = []
-    owners = []
-    for a, owned in purse.items():
-        for t in owned:
-            tokens.append(t)
-            owners.append(a)
-    assert len(tokens) == len(owners) == NUM_TOKENS
-    return vy_deployer.deploy("Token", owners, tokens)[0].\
-            deployments.get_contract_instance("Token")
+def tester():
+    class Tester:
+        time = 0
+        def mine(self, num_blocks):
+            self.time += 1
+    return Tester()
 
 
 @pytest.fixture
-def rootchain(vy_deployer, token):
-    return vy_deployer.deploy("RootChain", token.address)[0].\
-            deployments.get_contract_instance("RootChain")
+def rootchain():
+
+    class RootChain:
+        deposits = []
+
+        def deposit(self, token):
+            self.deposits.append(token)
+
+        def withdraw(self, token):
+            self.deposits.remove(token)
+
+    return RootChain()
 
 
 @pytest.fixture
-def operator(w3, rootchain):
-    return Operator(w3.eth.accounts[0], rootchain)
+def operator(rootchain):
+
+    class Operator:
+
+        def is_tracking(self, token):
+            return token in rootchain.deposits
+
+    return Operator()
 
 
 @pytest.fixture
-def users(w3, token, rootchain, operator, purse):
-    return [User(a, token, rootchain, operator, purse[a]) \
-            for a in w3.eth.accounts[1:]]
+def users(tester, rootchain, operator):
+    class User:
+
+        def __init__(self, tokens):
+            self.purse = {}
+            self.purse['eth'] = tokens
+            self.purse['plasma'] = []
+
+        def deposit(self, token):
+            self.purse['eth'].remove(token)
+            rootchain.deposit(token)
+            self.purse['plasma'].append(token)
+
+        def receive(self, token):
+            self.purse['plasma'].append(token)
+
+        def send(self, user, token):
+            self.purse['plasma'].remove(token)
+            user.receive(token)
+
+        def withdraw(self, token):
+            self.purse['plasma'].remove(token)
+            rootchain.withdraw(token)
+            token.exit_started = tester.time
+            self.purse['eth'].append(token)
+            
+    return [User([Token(1)]), User([])]
--- tests/test_normal.py
+++ tests/test_normal.py
@@ -1,25 +1,44 @@
 # Test normal operation of the Plasma chain (entries and exits)
+PLASMA_SYNC_PERIOD = 0
+PLASMA_WITHDRAW_PERIOD = 0
 
-def test_deposit(rootchain, users):
-    u = users[0]
-    total_coins = len(u.eth_purse)
-    u.deposit()
-    assert len(u.eth_purse) == total_coins - 1
-    assert len(u.plasma_purse) == 1
-    e = rootchain.events.Deposit
 
-def test_immediate_withdraw(rootchain, users):
+def test_deposit(tester, operator, users):
     u = users[0]
-    total_coins = len(u.eth_purse)
-    u.deposit()
-    u.withdraw()
-    assert len(u.eth_purse) == total_coins
+    token = u.purse['eth'][0]
+    # The operator isn't tracking our token yet
+    assert not operator.is_tracking(token)
     
-def test_1trade_withdraw(rootchain, users):
+    deposit_time = tester.time
+    u.deposit(token)
+    assert token not in u.purse['eth']
+    # Token is no longer in our eth purse
+    # Wait for operator to see it and start tracking
+    while not operator.is_tracking(token):
+        assert tester.time < deposit_time + PLASMA_SYNC_PERIOD
+        tester.mine(0)
+    # Trading is now live
+    assert token in u.purse['plasma']
+
+def test_immediate_withdraw(tester, operator, users):
+    u = users[0]
+    token = u.purse['eth'][0]
+    u.deposit(token)
+    assert operator.is_tracking(token)
+    u.withdraw(token)
+    # Wait until the challenge period has elapsed
+    while token.exit_started + PLASMA_WITHDRAW_PERIOD < tester.time:
+        tester.mine(0)
+    assert token in u.purse['eth']
+    assert not operator.is_tracking(token)
+    
+def test_1trade_withdraw(tester, operator, users):
     u1, u2 = users[:2]
-    u1.deposit()
-    token = u1.plasma_purse[0]
-    u1.send(u2)
-    assert token in u2.plasma_purse
-    u2.withdraw()
-    assert token in u2.eth_purse
+    token = u1.purse['eth'][0]
+    u1.deposit(token)
+    # Wait for operator to signal trading is ready
+    while not operator.is_tracking(token):
+        tester.mine(0)
+    # Send it to a different user
+    u1.send(u2, token)
+    assert token in u2.purse['plasma']
