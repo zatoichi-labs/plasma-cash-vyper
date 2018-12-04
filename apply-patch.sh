8c6f0ca529b7f6a3675836610f905b527a63acfb
Bryant Eisenbach <fubuloubu@gmail.com>
2018-12-04 13:45:29 -0500

    Merged patches into one script

--- apply-patch.sh
+++ apply-patch.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/bash
 
 location=`pip show eth-tester | grep Location | sed 's/.*: //g'`
-patch -i py-evm.patch $location/evm/vm/forks/spurious_dragon/constants.py
-patch -i eth-tester.patch $location/eth_tester/backends/pyevm/main.py
+
+# Code size of RootChain is 46370, over the limit of 24576
+patch -i <(echo "
+13c13
+< EIP170_CODE_SIZE_LIMIT = 24577
+---
+> EIP170_CODE_SIZE_LIMIT = 245770  # FIXME 10x'd this
+") $location/evm/vm/forks/spurious_dragon/constants.py
+
+# Gas deployment costs are higher than 3.14mgas
+patch -i <(echo "
+74c74
+< GENESIS_GAS_LIMIT = 3141592
+---
+> GENESIS_GAS_LIMIT = 31415920 # FIXME 10x'd this
+") $location/eth_tester/backends/pyevm/main.py
