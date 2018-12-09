f6ba1037c59d340836fa65d48481190776712d82
Bryant Eisenbach <fubuloubu@gmail.com>
2018-12-09 15:57:09 -0500

    Added generated purses

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,11 @@
 import pytest
 
+from eth_utils import (
+    to_int,
+    to_bytes,
+    keccak,
+)
+
 from plasma_cash import (
     token_interface,
     rootchain_interface,
@@ -27,6 +33,26 @@ def tester(w3):
 
 
 @pytest.fixture
+def initial_owners(w3):
+    return w3.eth.accounts[1:3]  # should be divide into 64
+
+
+@pytest.fixture
+def initial_purse(initial_owners):
+    # We need to issue 64 tokens, so allocate 64/N tokens
+    issue_per_user = 64//len(initial_owners)
+    assert issue_per_user*len(initial_owners) == 64
+    purse = {}
+    for a in initial_owners:
+        purse[a] = [
+                # First 4 bytes of the hash of user's address and a "random" num
+                to_int(keccak(to_bytes(hexstr=a) + to_bytes(i))[:4])
+                for i in range(issue_per_user)
+            ]
+    return purse
+
+
+@pytest.fixture
 def token(w3):
     owners = []
     for a in w3.eth.accounts[1:9]:
@@ -48,13 +74,23 @@ def rootchain(w3, token):
 
 @pytest.fixture
 def operator(w3, rootchain):
-    return Operator(w3, rootchain)
+    return Operator(w3, rootchain)#, w3.eth.accounts[0])
 
 
 @pytest.fixture
-def users(w3, rootchain, operator):
-    return [
-        User(w3, rootchain, operator, 1, {'eth': [Token(1)]}),
-        User(w3, rootchain, operator, 2),
-        User(w3, rootchain, operator, 3)
-    ]
+def users(w3, rootchain, operator, initial_owners, initial_purse):
+    users = []
+    # Add users with tokens
+    for a in initial_owners:
+        users.append(
+                User(w3,
+                     rootchain,
+                     operator,
+                     a,
+                     {'eth': [ Token(uid) for uid in initial_purse[a] ]},
+                )
+            )
+    # Add users without any tokens
+    for a in w3.eth.accounts[1+len(initial_owners):]:
+        users.append(User(w3, rootchain, operator, a))
+    return users
