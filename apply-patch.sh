5122213c445e4366353b81845f6fdfd5ffeef918
Bryant Eisenbach <fubuloubu@gmail.com>
2018-12-12 13:09:51 -0500

    py-evm changed their package; still having problems with genesis gas limit

--- apply-patch.sh
+++ apply-patch.sh
@@ -8,12 +8,12 @@ patch -i <(echo "
 < EIP170_CODE_SIZE_LIMIT = 24577
 ---
 > EIP170_CODE_SIZE_LIMIT = 245770  # FIXME 10x'd this
-") $location/evm/vm/forks/spurious_dragon/constants.py
+") $location/eth/vm/forks/spurious_dragon/constants.py
 
 # Gas deployment costs are higher than 3.14mgas
 patch -i <(echo "
-74c74
+81c81
 < GENESIS_GAS_LIMIT = 3141592
 ---
-> GENESIS_GAS_LIMIT = 31415920 # FIXME 10x'd this
+> GENESIS_GAS_LIMIT = 314159200 # FIXME 100x'd this
 ") $location/eth_tester/backends/pyevm/main.py
