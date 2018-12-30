43f1675b805b60108bdb33b21ea783a355ea48fe
Bryant Eisenbach <fubuloubu@gmail.com>
2018-12-30 13:05:05 -0500

    Vyper b6 lowered bytecode size to 90% of EIP170 limit, and deployment gas cost to 6.2m

--- apply-patch.sh
+++ apply-patch.sh
@@ -2,18 +2,10 @@
 
 location=`pip show eth-tester | grep Location | sed 's/.*: //g'`
 
-# Code size of RootChain is 46370, over the limit of 24576
-patch -i <(echo "
-13c13
-< EIP170_CODE_SIZE_LIMIT = 24577
----
-> EIP170_CODE_SIZE_LIMIT = 245770  # FIXME 10x'd this
-") $location/eth/vm/forks/spurious_dragon/constants.py
-
 # Gas deployment costs are higher than 3.14mgas
 patch -i <(echo "
 81c81
 < GENESIS_GAS_LIMIT = 3141592
 ---
-> GENESIS_GAS_LIMIT = 314159200 # FIXME 100x'd this
+> GENESIS_GAS_LIMIT = 6283184 # FIXME 2x'd this from 3141592
 ") $location/eth_tester/backends/pyevm/main.py
