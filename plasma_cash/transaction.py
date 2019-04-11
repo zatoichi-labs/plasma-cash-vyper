d0fb740dab136d56ee211f62c388d3527dbefbd1
Bryant Eisenbach <fubuloubu@gmail.com>
2019-04-10 20:40:10 -0400

    feat: Add EIP-712 transaction encoding [WIP]

--- plasma_cash/transaction.py
+++ plasma_cash/transaction.py
@@ -1,44 +1,97 @@
 from eth_abi import encode_single
 from eth_account import Account
+from eth_account.messages import defunct_hash_message
 from eth_utils import keccak, to_checksum_address
+
+
+def is_signature(val):
+    if not isinstance(signature, tuple):
+        return False
+    if len(signature) != 3:
+        return False
+    if not isinstance(signature[0], int):
+        return False
+    if not isinstance(signature[1], int):
+        return False
+    if not isinstance(signature[2], int):
+        return False
+    return True
 
 
 class Transaction:
 
     def __init__(self,
-            prevBlkNum,
-            tokenId,
-            newOwner,
-            sigV,
-            sigR,
-            sigS):
-        self.newOwner = newOwner
-        self.tokenId = tokenId
-        self.prevBlkNum = prevBlkNum
-        self.signature = (sigV, sigR, sigS)
-
-    @staticmethod
-    def unsigned_txn_hash(
+            chain_id,
+            contract_address,
             prevBlkNum,
             tokenId,
             newOwner):
-        data = encode_single('(uint256,uint256,address)', (prevBlkNum, tokenId, newOwner))
-        return keccak(data)
+        self.chain_id = chain_id
+        self.contract_address = contract_address
+        self.newOwner = newOwner
+        self.tokenId = tokenId
+        self.prevBlkNum = prevBlkNum
+        self.signature = None
+
+    def add_signature(self, signature):
+        assert is_signature(signature), "Not a valid signature!"
+        assert self.signature is None, "Message has already been signed!"
+        self.signature = signature
 
     @property
-    def sender(self):
-        data = encode_single('(uint256,uint256,address)', (self.prevBlkNum, self.tokenId, self.newOwner))
-        return Account.recoverHash(keccak(data), vrs=self.signature)
+    def _eip712_msg(self):
+        return {
+            "types": {
+                "EIP712Domain": [
+                    {"name": "name", "type": "string"},
+                    {"name": "version", "type": "string"},
+                    {"name": "chainId", "type": "uint256"},
+                    {"name": "verifyingContract", "type": "address"}
+                ],
+                "Transaction": [
+                    {"name":"newOwner", "type":"address"},
+                    {"name":"tokenId", "type":"uint256"},
+                    {"name":"prevBlkNum", "type":"uint256"},
+                ]
+            },
+            "primaryType": "Transaction",
+            "domain": {
+                "name": "Plasma Cash",
+                "version": "1",
+                "chainId": self.chain_id,
+                "verifyingContract": self.contract_address
+            },
+            "message": {
+                "newOwner": self.newOwner,
+                "tokenId": self.tokenId,
+                "prevBlkNum": self.prevBlkNum
+            }
+        }
+
+    @property
+    def msg_hash(self):
+        """ This is the message hash we sign for L2 transfers """
+        return defunct_hash_message(text=self._eip712_msg, signature_version=b'\x01')
+
+    @property
+    def signer(self):
+        """ Get the signing account for this transaction """
+        assert self.signature is not None, "Transaction is not signed!"
+        return Account.recoverHash(self.msg_hash, vrs=self.signature)
 
     @property
     def to_tuple(self):
+        """ This is how we pass a struct through eth-abi for interacting with L1 """
+        assert self.signature is not None, "Transaction is not signed!"
         return (self.prevBlkNum, self.tokenId, self.newOwner, *self.signature)
 
     @property
     def to_bytes(self):
-        return encode_single('(uint256,uint256,address,uint256,uint256,uint256)', self.to_tuple)
-
-    @property
-    def hash(self):
-        # Encode struct as ordered tuple of members
-        return keccak(self.to_bytes)
+        """ This is how we pass a message through p2p channels """
+        # Note we don't encode the domain separator, the receiver
+        # must validate that each transaction is signed according
+        # to the domain separator they are using.
+        return encode_single(
+                '(uint256,uint256,address,uint256,uint256,uint256)',
+                self.to_tuple
+            )
